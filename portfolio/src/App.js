0a2fd531b9088a8ffa1093b63dd1ea8992deed45
DHKim <dh95229505@gmail.com>
2024-10-31 22:25:07 +0900

    feat: Intro 기능 추가

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from "react";
-import{BrowserRouter, Router, Routes} from "react-router-dom";
+import{BrowserRouter, Route, Routes} from "react-router-dom";
+import HomeView from "./views/HomeView";
 
 function App() {
 
@@ -27,7 +28,9 @@ function App() {
 
   return(
     <BrowserRouter>
-    
+      <Routes>
+        <Route path="/" element={<HomeView/>}/>
+      </Routes>
     </BrowserRouter>
   );
 }
