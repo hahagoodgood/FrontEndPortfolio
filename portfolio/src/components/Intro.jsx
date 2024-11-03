43d7de273e605afaf80745ea2527b571653c270e
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2024-11-03 16:18:16 +0900

    feat: conatct 기능추가

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -10,7 +10,7 @@ const introText = {
 const Intro = () => {
     return (
         <section id="intro">
-            <div class="intro__inner">
+            {/* <div class="intro__inner">
                 <h1 class="intro__title">{introText.title}</h1>
                 <div class="intro__lines" aria-hidden="true">
                     <span class="line"></span>
@@ -40,7 +40,7 @@ const Intro = () => {
                     <span class="line"></span>
                     <span class="line"></span>
                 </div>
-            </div>
+            </div> */}
         </section>
     );
 };
