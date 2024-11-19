03472defcdc0a60106ebc08a59131b98f98df51e
kimdonghyuk <144420387+hahagoodgood@users.noreply.github.com>
2024-11-19 11:39:52 +0900

    style: 배경색 수정

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -67,7 +67,6 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer__right">
-                    © 2023 webstoryboy<br />
                     이 사이트는 리액트를 이용하여 제작하였습니다.
                 </div>
             </div>
